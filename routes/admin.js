2d2e043a98464aebd56f5e1db3fe90109e79d2d6
Ajay T <ajaythekkiniyilmaniyara@gmail.com>
2020-11-28 22:42:26 +0530

    admin login page modified

--- routes/admin.js
+++ routes/admin.js
@@ -5,17 +5,26 @@ var dbo = require("../config/connection");
 var adminHelper = require("../helper/adminHelper");
 var session = require("express-session");
 
-/* GET admin login page */
-router.get("/", function (req, res, next) {
+// middileware checking if admin login or not
+var ifLogin = (req, res, next) => {
   if (req.session.adminLogedin) {
-    res.redirect("admin/adminPanel");
-  }
-  if (req.session.loggedErr) {
+    next();
+  } else if (req.session.loggedErr) {
     let loggedErr = req.session.loggedErr;
     res.render("admin/adminLogin", { loggedErr });
   } else {
-    res.render("admin/adminLogin");
+    next();
   }
+};
+
+
+/* GET admin login page */
+router.get("/", ifLogin, function (req, res, next) {
+  if (req.session.adminLogedin) {
+    res.redirect('admin/adminPanel');
+  }
+  else
+     res.render("admin/adminLogin");
 });
 
 // Admin panel page
@@ -27,12 +36,13 @@ router.get("/adminPanel", (req, res) => {
     res.redirect("/admin");
   }
 });
+// admin logout
 router.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/admin");
 });
 
-//admin credentials verification
+//admin login- credentials verification
 router.post("/verify", (req, res) => {
   adminHelper.verifyAdminLogin(req.body).then((resp) => {
     if (resp.logedin == true) {
@@ -48,34 +58,31 @@ router.post("/verify", (req, res) => {
 
 router.get("/dashboard", (req, res) => {
   let adminName = req.session.adminName;
-  res.render("admin/adminPanel",{ adminName });
+  res.render("admin/adminPanel", { adminName });
 });
 router.get("/doctors", (req, res) => {
   let adminName = req.session.adminName;
-  res.render("admin/adminDoctors",{ adminName });
+  res.render("admin/adminDoctors", { adminName });
 });
 router.get("/appointments", (req, res) => {
   let adminName = req.session.adminName;
-  res.render("admin/adminAppointment",{ adminName });
+  res.render("admin/adminAppointment", { adminName });
 });
 router.get("/patients", (req, res) => {
   let adminName = req.session.adminName;
-  res.render("admin/adminPatients",{ adminName });
+  res.render("admin/adminPatients", { adminName });
 });
 
-
 //GET add doctors
-router.get('/adddoctors',(req,res)=>{
+router.get("/adddoctors", (req, res) => {
   let adminName = req.session.adminName;
-  res.render("admin/addDoctors",{ adminName });
-})
-
+  res.render("admin/addDoctors", { adminName });
+});
 
 //adding doctors details to database
-router.post('/addDoctorData',(req,res)=>{
-  adminHelper.addDoctorData(req.body)
-  res.send('doctors added')
-})
-
+router.post("/addDoctorData", (req, res) => {
+  adminHelper.addDoctorData(req.body);
+  res.send("doctors added");
+});
 
 module.exports = router;
